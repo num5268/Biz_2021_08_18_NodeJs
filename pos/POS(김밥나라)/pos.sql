357c8af5e2e94f717ee782267bd6e0afb33971d4
num5268 <num5268@gmail.com>
2021-08-30 09:40:15 +0900

    num5268

--- pos/POS(김밥나라)/pos.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'gimbap'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'gimbap'@'%';
-CREATE DATABASE gimbapDB;
-USE gimbapDB;
-
-DROP TABLE tbl_products;
-DROP TABLE tbl_orders;
