1733bc6971328d14dfc01988dbe0e3f6e1429070
num5268 <num5268@gmail.com>
2021-08-30 10:02:39 +0900

    num5268

--- OneDayJS_0827/models/tbl_product.js
+++ OneDayJS_0827/models/tbl_product.js
@@ -1,32 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-    const product = sequelize.define(
-      "tbl_product",
-      {
-        p_code : {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        p_name :{
+  const product = sequelize.define(
+    "tbl_product",
+    {
+      p_code: {
+        type: DataTypes.STRING(10),
+        primaryKey: true,
+      },
+      p_name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      p_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      p_rem: { type: DataTypes.STRING(255) },
+    },
+    { timestamps: false }
+  );
 
-        },
-        p_price :{
-
-        },
-        p_rem :{
-
-        },
-
-      // createAt, updateAt 만들지 않기
-      { timestamps: false }
-    );
-  
-    // bbs(tbl_bbs table)과 tbl_replay 를 1:N 관계로 설정하기
-    // 이때 tbl_bbs.b_id 칼럼과 tbl_replay.r_postId 칼럼을
-    // 연관지어 FK 설정하라
-    bbs.associate = (models) => {
-      bbs.hasMany(models.tbl_reply, { foreignKey: "r_postId" });
-    };
-    return bbs;
-  };
-  
+  return product;
+};
